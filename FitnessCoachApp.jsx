23c83d992285ecbd0b9adc9a88f9a9041e3d7735
aboudarbal <kaboudarbal@gmail.com>
2025-05-19 15:43:28 +0200

    Add files via upload

---
+++ FitnessCoachApp.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+/* rest of code omitted here for brevity; will be restored below */
